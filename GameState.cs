94f207f7b7a15940c707a00564a32add8678fc27
zainab7681051 <zainab76810510@gmail.com>
2023-09-19 22:33:15 +0300

    commit

--- GameState.cs
+++ GameState.cs
@@ -13,7 +13,8 @@ namespace ClassicSnakeGameGUI
         public int Score { get; private set; }
         public bool GameOver { get; private set; }
 
-        public readonly LinkedList<Position> snakePositions = new();
+        private readonly LinkedList<Position> snakePositions = new();
+        private readonly LinkedList<Direction> dirChanges = new();
         private readonly Random random = new();
 
 
@@ -78,9 +79,29 @@ namespace ClassicSnakeGameGUI
             Grid[tail.Row, tail.Col] = GridValue.Empty;
             snakePositions.RemoveLast();
         }
+        private Direction GetLastDirection()
+        {
+            if (dirChanges.Count == 0)
+            {
+                return Dir;
+            }
+            return dirChanges.Last.Value;
+        }
+        private bool CanChangeDir(Direction newDir)
+        {
+            if (dirChanges.Count == 2)
+            {
+                return false;
+            }
+            Direction lastDir = GetLastDirection();
+            return newDir != lastDir && newDir != lastDir.Opposite();
+        }
         public void Changedirection(Direction dir)
         {
-            Dir = dir;
+            if (CanChangeDir(dir))
+            {
+                dirChanges.AddLast(dir);
+            }
         }
 
         private bool OutsideGrid(Position pos)
@@ -107,6 +128,11 @@ namespace ClassicSnakeGameGUI
         }
         public void Move()
         {
+            if (dirChanges.Count > 0)
+            {
+                Dir = dirChanges.First.Value;
+                dirChanges.RemoveFirst();
+            }
             Position newHeadPos = HeadPosition().Translate(Dir);
             GridValue hit = WillHit(newHeadPos);
             if (hit == GridValue.Outside || hit == GridValue.Snake)
